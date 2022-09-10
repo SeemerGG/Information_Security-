e9956224a24fb4d111666e26d149246d344e7b51
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-09-11 00:52:36 +0300

    Сделал вторую лабораторную работу

---
+++ Practically Lesson2/Practically Lesson2/Practically Lesson2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sum_digit(int i)
+{
+	int b = i;
+	int sum = 0;
+
+	while (b != 0)
+	{
+		sum += b % 10;
+		b /= 10;
+	}
+	return sum;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << "Введите очтавшиеся цифры:";
+
+	int digit;
+	cin >> digit;
+	int sum_d = sum_digit(digit);
+	int res = sum_d;
+	while (res % 9 != 0)
+	{
+		res++;
+	}
+
+	cout << "Зачеркнутая цифра:" << res - sum_d << endl;
+}
