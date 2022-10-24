1ea03a0c2f3503956d24d96ad7abe31b434e237b
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-10-24 20:21:26 +0300

    Сделал задание 7.2

--- Practically_Lesson_7_HRU/Practically_Lesson_7_HRU/Practically_Lesson_7_HRU.cpp
+++ Practically_Lesson_7_HRU/Practically_Lesson_7_HRU/Practically_Lesson_7_HRU.cpp
@@ -118,10 +118,133 @@ private:
         fout.close();
     }
 
+    //Задание 2
+    vector<int> a()
+    {
+        bool flag;
+        vector<int> buf;
+        for (int j = 0; j < m; j++)
+        {
+            flag = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (!A[i][j].empty())
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                buf.push_back(j);
+            }
+        }
+        return buf;
+    }
+
+    vector<int> b()
+    {
+        bool flag;
+        vector<int> buf;
+        for (int i = 0; i < n; i++)
+        {
+            flag = true;
+            for (int j = 0; j < m; j++)
+            {
+                if (!A[i][j].empty())
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                buf.push_back(i);
+            }
+        }
+        return buf;
+    }
+
+    vector<int> c()
+    {
+        bool flag;
+        vector<int> buf;
+        for (int i = 0; i < n; i++)
+        {
+            flag = true;
+            for (int j = 0; j < m; j++)
+            {
+                if (A[i][j].empty() || (A[i][j].contains('w') && !A[i][j].contains('r')) || (!A[i][j].contains('w') && A[i][j].contains('r')))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                buf.push_back(i);
+            }
+        }
+        return buf;
+    }
+
+    vector<vector<int>> d()
+    {
+        vector<vector<int>> buf;
+        for (int j = 0; j < m; j++)
+        {
+            vector<int> buf1;
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i][j].contains('w'))
+                {
+                    buf1.push_back(i);
+                }
+            }
+            buf.push_back(buf1);
+        }
+        return buf;
+    }
+
+    vector<int> e()
+    {
+        bool flag;
+        vector<int> buf;
+        for (int i = 0; i < n; i++)
+        {
+            flag = true;
+            int count = 0;
+            for (int j = 0; j < m; j++)
+            {
+                if (A[i][j].contains('w') && A[i][j].contains('r'))
+                {
+                    count++;
+                }
+                else if (A[i][j].contains('w'))
+                {
+                    flag = false;
+                }
+            }
+            if (flag && count == 1)
+            {
+                buf.push_back(i);
+            }
+        }
+        return buf;
+    }
+    void output_vector_int(vector<int> buf)
+    {
+        for (int i = 0; i < buf.size(); i++)
+        {
+            cout << buf[i] << " ";
+        }
+        cout << endl;
+    }
+
 public:
     HRU(string file_name) //файл оканчивается табуляцией 
     {   
-        ifstream fin("environ.txt");
+        ifstream fin(file_name);
         if (fin.is_open())
         {
             fin >> n;
@@ -218,12 +341,36 @@ public:
         }
     }
 
+    void task2()
+    {
+        vector<int> buf = a();
+        cout << "a) ";
+        output_vector_int(buf);
+        buf = b();
+        cout << "b) ";
+        output_vector_int(buf);
+        buf = c();
+        cout << "c) ";
+        output_vector_int(buf);
+        vector<vector<int>> buf2 = d();
+        cout << "d) " << endl;
+        for (int i = 0; i < buf2.size(); i++)
+        {
+            output_vector_int(buf2[i]);
+        }
+        buf = e();
+        cout << "e) ";
+        output_vector_int(buf);
+    }
+
 };
 
 int main()
 {
-    string str = "environ.txt";
-    HRU hru1(str);
+    HRU hru1("environ.txt");
     hru1.interpretator("prgrm.txt");
     hru1.output();
+    HRU ne_hru("access_matr.txt");
+    ne_hru.task2();
+    
 }
