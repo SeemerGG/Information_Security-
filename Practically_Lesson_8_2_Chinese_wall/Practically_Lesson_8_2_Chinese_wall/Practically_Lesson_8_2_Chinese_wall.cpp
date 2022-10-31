c1fc0defda3ba7ff6317518d0f73472697e6ec27
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2022-10-31 15:28:11 +0300

    Сделал задание 8.2

--- Practically_Lesson_8_2_Chinese_wall/Practically_Lesson_8_2_Chinese_wall/Practically_Lesson_8_2_Chinese_wall.cpp
+++ Practically_Lesson_8_2_Chinese_wall/Practically_Lesson_8_2_Chinese_wall/Practically_Lesson_8_2_Chinese_wall.cpp
@@ -10,10 +10,22 @@ class chinese_wall
 private:
 
     int n, m, f_count;
-    vector<set<int>> f;
-    map<int, set<int>> cl;
+    map<string, set<int>> f;
+    vector<set<string>> cl;
+    map<pair<int, int>, set<char>> history;
+    
 
 public:
+
+    chinese_wall(int n, int m, int f_count, map<string, set<int>> f, vector<set<string>> cl)
+    {
+        this->n = n;
+        this->m = m;
+        this->f = f;
+        this->cl = cl;
+        this->f_count;
+    }
+
     chinese_wall(string file_name)
     {
         try
@@ -22,40 +34,36 @@ public:
             if (fin.is_open())
             {
                 fin >> n >> m >> f_count;
+                string name_company;
                 set<int> buf;
-                int num;
-                for (int i = 0; i < f_count; i++)
+                int num_obj;
+                for (int i = 0; i < f_count;i++)
                 {
-                    for (int j = 0; j < m; j++)
+                    fin >> name_company;
+                    while(fin.get() != '\n')
                     {
-                        fin >> num;
-                        buf.insert(num);
+                        fin >> num_obj;
+                        buf.insert(num_obj);
                     }
-                    f.push_back(buf);
+                    f[name_company] = buf;
                     buf.clear();
                 }
-                fin.ignore(1);
-                char buf1;
-                int  num_sbj = 0;
-                while (!fin.eof())
+                set<string> buf1;
+                int cl_count;
+                fin >> cl_count;
+                if (cl_count != 0)
                 {
-                    fin.get(buf1);
-                    if (buf1 == '\n')
+                    for (int i = 0; i < cl_count; i++)
                     {
-                        cl[num_sbj] = buf;
-                        buf.clear();
-                        num_sbj++;
-                    }
-                    else
-                    {
-                        if (buf1 != ' ')
+                        fin >> name_company;
+                        buf1.insert(name_company);
+                        while (fin.get() != '\n')
                         {
-                            buf.insert(buf1 - '0');
+                            fin >> name_company;
+                            buf1.insert(name_company);
                         }
-                        else
-                        {
-                            fin.ignore(1);
-                        }
+                        cl.push_back(buf1);
+                        buf1.clear();
                     }
                 }
             }
@@ -72,11 +80,161 @@ public:
         }
     }
 
+    void start()
+    {
+        history.clear();
+    }
+
+    bool check_read(int i, int j)
+    {
+        pair<int, int> pair1(pair(i, j));
+        if (history.contains(pair1))
+        {
+            return true;
+        }
+        else
+        {
+            string name_com_j = get_name_company(j);
+            string name_com_k;
+            for (int k = 0; k < m; k++)
+            {
+                name_com_k = get_name_company(k);
+                if (history.contains(pair(i, k)))
+
+                {
+                    if (name_com_k != name_com_j && get_num_cl(name_com_j) == get_num_cl(name_com_j))
+                    {
+                        return false;
+                    }
+                    if (name_com_k == name_com_j)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    void read(int i, int j)
+    {
+        if (check_read(i, j))
+        {
+            history[pair(i, j)].insert('r');
+            cout << "accepted\n";
+        }
+        else
+        {
+            cout << "refused\n";
+        }
+    }
+
+    string get_name_company(int j)
+    {
+        try
+        {
+            for (auto k : f)
+            {
+                if (k.second.contains(j))
+                {
+                    return k.first;
+                }
+            }
+        }
+        catch (...)
+        {
+            cout << "Error!";
+            terminate();
+        }
+        
+    }
+
+    int get_num_cl(string name_com)
+    {
+        try 
+        {
+            for (int i = 0; i < cl.size(); i++)
+            {
+                if (cl[i].contains(name_com))
+                {
+                    return i;
+                }
+            }
+        }
+        catch (...)
+        {
+            cout << "Error!";
+            terminate();
+        }
+    }
+
+    void write(int i, int j)
+    {
+        if (check_read(i, j))
+        {
+            history[pair(i, j)].insert('w');
+            cout << "accepted\n";
+        }
+        else
+        {
+            cout << "refused\n";
+        }
+    }
+
+    void report_sbj(int i)
+    {
+        pair<int, int> pair1;
+        for (int j = 0; j < m; j++)
+        {   
+            pair1 = pair(i, j);
+            if (history.contains(pair1))
+            {
+                string name_com = get_name_company(j);
+                for (auto k : history[pair1])
+                {
+                    cout << k << " - " << "Obj number: " << j << " from " << name_com << endl;
+                }
+            }
+        }
+    }
+
+    void report_obj(int j)
+    {
+        pair<int, int> pair1;
+        for (int i = 0; i < n; i++)
+        {
+            pair1 = pair(i, j);
+            if (history.contains(pair1))
+            {
+                for (auto k : history[pair1])
+                {
+                    cout << "Sbj number " << i << " - " << k << endl;
+                }
+            }
+        }
+    }
+
+    void report_brief_case(string name_com)
+    {
+        for (auto i : f[name_com])
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 
 };
 int main()
 {
-    
+    chinese_wall cw1("input1");
+
+    cw1.report_brief_case("GasProm");
+    cw1.read(1, 1);
+    cw1.write(1, 1);
+    cw1.write(1, 2);
+    cw1.report_sbj(1);
+    cw1.report_obj(1);
+    cw1.start();
+    cw1.write(1, 2);
+    cw1.report_sbj(1);
 }
-
-
